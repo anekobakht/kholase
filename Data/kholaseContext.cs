27646965363eb3f89a1a3d12a7ece3f72e9ae6b2
Alireza Nekobakht <a.nekobakht17439abcd@gmail.com>
2023-02-03 20:59:40 +0330

    first commit

---
+++ Data/kholaseContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using kholase.Models;
+
+namespace kholase.Data
+{
+    public class kholaseContext : DbContext
+    {
+        public kholaseContext(DbContextOptions<kholaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<kholase.Models.user>? user { get; set; } = default!;
+        public DbSet<kholase.Models.never>? never { get; set; }
+        public DbSet<kholase.Models.v_nbt_sms2>? v_nbt_sms2 { get; set; }
+        public DbSet<kholase.Models.v_nbt_sms1>? v_nbt_sms1 { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.Entity<v_nbt_sms3>()
+            //.ToView(nameof(v_nbt_sms2))
+            //.HasNoKey();
+
+           // modelBuilder.Entity<v_nbt_sms2>()
+           //.ToView(nameof(v_nbt_sms2))
+           //.HasNoKey();
+
+        }
+
+    }
+}
---
+++ Models/user.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kholase.Models
+{
+    public class user
+    {
+        [Key]
+        public Int64 id_user { get; set; }
+        //**********************************************************************
+        [Display(Name = "نام و نام خانوادگی")]
+        [Required(ErrorMessage = "لطفا نام و نام خانوادگی را وارد نمایید")]
+        public string name { get; set; }
+        //**********************************************************************
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا نام کاربری را وارد نمایید")]
+        public string u_name { get; set; }
+        //**********************************************************************
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا کلمه عبور را وارد نمایید")]
+        public string u_pass { get; set; }
+        //**********************************************************************
+        [Display(Name = "مدیر سیستم")]
+        public bool admin { get; set; }
+        //**********************************************************************
+
+
+    }
+}
